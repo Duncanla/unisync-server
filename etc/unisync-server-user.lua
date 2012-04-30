7b2c0e961ba05b4eeef30f536b01884d95e0ec3f
Luke Duncan <Duncan72187@gmail.com>
2012-04-30 18:50:19 -0400

    Removed values from the configuration file that were used for testing. Now it just holds a skeleton configuration.

--- etc/unisync-server-user.lua
+++ etc/unisync-server-user.lua
@@ -2,7 +2,7 @@
 -- Watch for updates that are pushed to the server
 sync {
    server_update,
-   unisync_id = "data_sync",
-   source = "/home/luke/testunisync",
+   unisync_id = "",
+   source = "",
    target = localhost
 }
