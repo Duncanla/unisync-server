23e728afe7f671c90bf1936adc7275a1e2a1c259
Luke Duncan <Duncan72187@gmail.com>
2012-04-30 21:34:10 -0400

    Removed target(=localhost) from the user configuration. Added documentation to the user configuration file.

--- etc/unisync-server-user.lua
+++ etc/unisync-server-user.lua
@@ -1,8 +1,19 @@
 
+-- Unisync server user configuration
+-- Add syncs below
+--
+-- server_update: This is the default unisync mode. Leave it as it
+--                is.
+--
+-- unisync_id:    This is a unique id for each sync that will be used
+--                by the client to identify which sync it wants to use
+--
+-- source:        This is the directory that you would like to sync
+ 
+
 -- Watch for updates that are pushed to the server
 sync {
    server_update,
    unisync_id = "",
    source = "",
-   target = localhost
 }
